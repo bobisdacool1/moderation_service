8d09aad57c1626b5838605ac2930c98e7648a751
sinitsyn.am <sinitsin.aleksey2003@yandex.ru>
2025-06-18 17:36:20 +0300

    Add kafka to healthcheck
    (cherry picked from commit 4b92c2aa08324fde5e235bced355ed928b6805af)

--- internal/delivery/http/handler/healthcheck.go
+++ internal/delivery/http/handler/healthcheck.go
@@ -1,19 +1,33 @@
 package handler
 
 import (
-	"net/http"
+	"context"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 type (
-	HealthcheckHandler struct{}
+	HealthcheckHandler struct {
+		healthcheckUsecase HealthcheckUsecase
+	}
+
+	HealthcheckUsecase interface {
+		Healthcheck(ctx context.Context) error
+	}
 )
 
-func NewHealthcheck() *HealthcheckHandler {
-	return &HealthcheckHandler{}
+func NewHealthcheckHandler(usecase HealthcheckUsecase) *HealthcheckHandler {
+	return &HealthcheckHandler{
+		healthcheckUsecase: usecase,
+	}
 }
 
 func (h *HealthcheckHandler) Healthcheck(c *fiber.Ctx) error {
-	return c.SendStatus(http.StatusOK)
+	err := h.healthcheckUsecase.Healthcheck(c.Context())
+	if err != nil {
+		return fiber.NewError(fiber.StatusInternalServerError, err.Error())
+	}
+
+	c.Status(fiber.StatusOK)
+	return nil
 }
